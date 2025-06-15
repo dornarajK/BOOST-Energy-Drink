27eef522bda28a174a61179af192acf65ae71863
dornarajK <devraj.kharal05@gmail.com>
2025-06-15 14:49:40 +0300

    linux-x64 runtime

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    unoptimized: true,  // hyvä tässä vaiheessa
+    disableStaticImages: true,
   },
-  /* config options here */
+  experimental: {
+    typedRoutes: true,  // (jos käytät App Routeria)
+  },
+  webpack: (config) => {
+    config.externals = [
+      ...(config.externals || []),
+      {
+        sharp: 'commonjs sharp',
+      },
+    ];
+    return config;
+  },
 };
 
 export default nextConfig;
